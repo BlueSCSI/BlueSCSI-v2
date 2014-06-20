ff05c3e856feb389f09c196a69f4b7addf1dea36
Michael McMaster <michael@codesrc.com>
2014-06-20 14:25:53 +1000

    3.5" Board redesign!
    - A few GPIO ports for expansion.
    - SD SPI signals brought to a header.
    - Reduced solder stencil holes for PSoC to reduce bridging
    - Increased solder stencil holes for micro SD to reduce cold joints
    - Added external LED support
    - 3.5" mounting hole positions fixed
    - Added 2.5" mounting holes for use with 2.5" to 3.5" bracket.

--- lib/SCSI2SD/hardware/scsi2sd.cmd
+++ lib/SCSI2SD/hardware/scsi2sd.cmd
@@ -1,5 +1,182 @@
 # Pin name action command file
 
+# Start of element J8
+ChangePinName(J8, 11, 11)
+ChangePinName(J8, 12, 12)
+ChangePinName(J8, 13, 13)
+ChangePinName(J8, 14, 14)
+ChangePinName(J8, 15, 15)
+ChangePinName(J8, 16, 16)
+ChangePinName(J8, 6, 6)
+ChangePinName(J8, 7, 7)
+ChangePinName(J8, 8, 8)
+ChangePinName(J8, 9, 9)
+ChangePinName(J8, 10, 10)
+ChangePinName(J8, 5, 5)
+ChangePinName(J8, 1, 1)
+ChangePinName(J8, 4, 4)
+ChangePinName(J8, 3, 3)
+ChangePinName(J8, 2, 2)
+
+# Start of element J11
+ChangePinName(J11, 8, 8)
+ChangePinName(J11, 7, 7)
+ChangePinName(J11, 6, 6)
+ChangePinName(J11, 5, 5)
+ChangePinName(J11, 4, 4)
+ChangePinName(J11, 3, 3)
+ChangePinName(J11, 2, 2)
+ChangePinName(J11, 1, 1)
+
+# Start of element J12
+ChangePinName(J12, 11, 11)
+ChangePinName(J12, 12, 12)
+ChangePinName(J12, 13, 13)
+ChangePinName(J12, 14, 14)
+ChangePinName(J12, 15, 15)
+ChangePinName(J12, 16, 16)
+ChangePinName(J12, 17, 17)
+ChangePinName(J12, 18, 18)
+ChangePinName(J12, 6, 6)
+ChangePinName(J12, 7, 7)
+ChangePinName(J12, 8, 8)
+ChangePinName(J12, 9, 9)
+ChangePinName(J12, 10, 10)
+ChangePinName(J12, 5, 5)
+ChangePinName(J12, 1, 1)
+ChangePinName(J12, 4, 4)
+ChangePinName(J12, 3, 3)
+ChangePinName(J12, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element Q1
+ChangePinName(Q1, S, S)
+ChangePinName(Q1, D, D)
+ChangePinName(Q1, G, G)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element J10
+ChangePinName(J10, 1, 1)
+ChangePinName(J10, 2, 2)
+
+# Start of element J7
+ChangePinName(J7, 1, 1)
+ChangePinName(J7, 2, 2)
+
+# Start of element D1
+ChangePinName(D1, 1, cathode)
+ChangePinName(D1, 2, anode)
+
+# Start of element J3
+ChangePinName(J3, 1, 1)
+ChangePinName(J3, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element J2
+ChangePinName(J2, 50, 50)
+ChangePinName(J2, 49, 49)
+ChangePinName(J2, 48, 48)
+ChangePinName(J2, 47, 47)
+ChangePinName(J2, 46, 46)
+ChangePinName(J2, 45, 45)
+ChangePinName(J2, 44, 44)
+ChangePinName(J2, 43, 43)
+ChangePinName(J2, 42, 42)
+ChangePinName(J2, 41, 41)
+ChangePinName(J2, 40, 40)
+ChangePinName(J2, 39, 39)
+ChangePinName(J2, 38, 38)
+ChangePinName(J2, 37, 37)
+ChangePinName(J2, 36, 36)
+ChangePinName(J2, 35, 35)
+ChangePinName(J2, 34, 34)
+ChangePinName(J2, 33, 33)
+ChangePinName(J2, 32, 32)
+ChangePinName(J2, 31, 31)
+ChangePinName(J2, 30, 30)
+ChangePinName(J2, 29, 29)
+ChangePinName(J2, 28, 28)
+ChangePinName(J2, 27, 27)
+ChangePinName(J2, 26, 26)
+ChangePinName(J2, 25, 25)
+ChangePinName(J2, 24, 24)
+ChangePinName(J2, 23, 23)
+ChangePinName(J2, 22, 22)
+ChangePinName(J2, 21, 21)
+ChangePinName(J2, 20, 20)
+ChangePinName(J2, 19, 19)
+ChangePinName(J2, 18, 18)
+ChangePinName(J2, 17, 17)
+ChangePinName(J2, 16, 16)
+ChangePinName(J2, 15, 15)
+ChangePinName(J2, 14, 14)
+ChangePinName(J2, 13, 13)
+ChangePinName(J2, 12, 12)
+ChangePinName(J2, 11, 11)
+ChangePinName(J2, 10, 10)
+ChangePinName(J2, 9, 9)
+ChangePinName(J2, 8, 8)
+ChangePinName(J2, 7, 7)
+ChangePinName(J2, 6, 6)
+ChangePinName(J2, 5, 5)
+ChangePinName(J2, 4, 4)
+ChangePinName(J2, 3, 3)
+ChangePinName(J2, 2, 2)
+ChangePinName(J2, 1, 1)
+
+# Start of element F1
+ChangePinName(F1, 2, 2)
+ChangePinName(F1, 1, 1)
+
+# Start of element D2
+ChangePinName(D2, 1, cathode)
+ChangePinName(D2, 2, anode)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element J1
+ChangePinName(J1, 4, +5V)
+ChangePinName(J1, 3, GND)
+ChangePinName(J1, 2, GND)
+ChangePinName(J1, 1, +12V)
+
+# Start of element J9
+ChangePinName(J9, 11, 11)
+ChangePinName(J9, 12, 12)
+ChangePinName(J9, 13, 13)
+ChangePinName(J9, 14, 14)
+ChangePinName(J9, 15, 15)
+ChangePinName(J9, 16, 16)
+ChangePinName(J9, 19, 19)
+ChangePinName(J9, 17, 17)
+ChangePinName(J9, 20, 20)
+ChangePinName(J9, 18, 18)
+ChangePinName(J9, 6, 6)
+ChangePinName(J9, 7, 7)
+ChangePinName(J9, 8, 8)
+ChangePinName(J9, 9, 9)
+ChangePinName(J9, 10, 10)
+ChangePinName(J9, 5, 5)
+ChangePinName(J9, 1, 1)
+ChangePinName(J9, 4, 4)
+ChangePinName(J9, 3, 3)
+ChangePinName(J9, 2, 2)
+
 # Start of element J6
 ChangePinName(J6, 10, \_CD\_)
 ChangePinName(J6, 9, GND)
@@ -59,29 +236,9 @@ ChangePinName(D3, 2, anode)
 ChangePinName(F2, 2, 2)
 ChangePinName(F2, 1, 1)
 
-# Start of element F1
-ChangePinName(F1, 2, 2)
-ChangePinName(F1, 1, 1)
-
-# Start of element D1
-ChangePinName(D1, 1, cathode)
-ChangePinName(D1, 2, anode)
-
-# Start of element D2
-ChangePinName(D2, 1, cathode)
-ChangePinName(D2, 2, anode)
-
-# Start of element J3
-ChangePinName(J3, 1, 1)
-ChangePinName(J3, 2, 2)
-
 # Start of element C14
 ChangePinName(C14, 2, 2)
 ChangePinName(C14, 1, 1)
-
-# Start of element C4
-ChangePinName(C4, 2, 2)
-ChangePinName(C4, 1, 1)
 
 # Start of element C7
 ChangePinName(C7, 2, 2)
@@ -215,34 +372,6 @@ ChangePinName(U1, 3, P2[7])
 ChangePinName(U1, 2, P2[6])
 ChangePinName(U1, 1, P2[5])
 
-# Start of element R12
-ChangePinName(R12, 1, 1)
-ChangePinName(R12, 2, 2)
-
-# Start of element R11
-ChangePinName(R11, 1, 1)
-ChangePinName(R11, 2, 2)
-
-# Start of element R10
-ChangePinName(R10, 1, 1)
-ChangePinName(R10, 2, 2)
-
-# Start of element R9
-ChangePinName(R9, 1, 1)
-ChangePinName(R9, 2, 2)
-
-# Start of element R8
-ChangePinName(R8, 1, 1)
-ChangePinName(R8, 2, 2)
-
-# Start of element R7
-ChangePinName(R7, 1, 1)
-ChangePinName(R7, 2, 2)
-
-# Start of element R6
-ChangePinName(R6, 1, 1)
-ChangePinName(R6, 2, 2)
-
 # Start of element C26
 ChangePinName(C26, 2, 2)
 ChangePinName(C26, 1, 1)
@@ -315,10 +444,6 @@ ChangePinName(J4, 4, 4)
 ChangePinName(J4, 3, 3)
 ChangePinName(J4, 2, 2)
 
-# Start of element C1
-ChangePinName(C1, 2, 2)
-ChangePinName(C1, 1, 1)
-
 # Start of element U5
 ChangePinName(U5, 8, \_Y3\_)
 ChangePinName(U5, 9, A3)
@@ -367,68 +492,6 @@ ChangePinName(U3, 3, A1)
 ChangePinName(U3, 2, \_Y0\_)
 ChangePinName(U3, 1, A0)
 
-# Start of element C2
-ChangePinName(C2, 2, 2)
-ChangePinName(C2, 1, 1)
-
 # Start of element C22
 ChangePinName(C22, 2, 2)
 ChangePinName(C22, 1, 1)
-
-# Start of element J1
-ChangePinName(J1, 4, +5V)
-ChangePinName(J1, 3, GND)
-ChangePinName(J1, 2, GND)
-ChangePinName(J1, 1, +12V)
-
-# Start of element J2
-ChangePinName(J2, 50, 50)
-ChangePinName(J2, 49, 49)
-ChangePinName(J2, 48, 48)
-ChangePinName(J2, 47, 47)
-ChangePinName(J2, 46, 46)
-ChangePinName(J2, 45, 45)
-ChangePinName(J2, 44, 44)
-ChangePinName(J2, 43, 43)
-ChangePinName(J2, 42, 42)
-ChangePinName(J2, 41, 41)
-ChangePinName(J2, 40, 40)
-ChangePinName(J2, 39, 39)
-ChangePinName(J2, 38, 38)
-ChangePinName(J2, 37, 37)
-ChangePinName(J2, 36, 36)
-ChangePinName(J2, 35, 35)
-ChangePinName(J2, 34, 34)
-ChangePinName(J2, 33, 33)
-ChangePinName(J2, 32, 32)
-ChangePinName(J2, 31, 31)
-ChangePinName(J2, 30, 30)
-ChangePinName(J2, 29, 29)
-ChangePinName(J2, 28, 28)
-ChangePinName(J2, 27, 27)
-ChangePinName(J2, 26, 26)
-ChangePinName(J2, 25, 25)
-ChangePinName(J2, 24, 24)
-ChangePinName(J2, 23, 23)
-ChangePinName(J2, 22, 22)
-ChangePinName(J2, 21, 21)
-ChangePinName(J2, 20, 20)
-ChangePinName(J2, 19, 19)
-ChangePinName(J2, 18, 18)
-ChangePinName(J2, 17, 17)
-ChangePinName(J2, 16, 16)
-ChangePinName(J2, 15, 15)
-ChangePinName(J2, 14, 14)
-ChangePinName(J2, 13, 13)
-ChangePinName(J2, 12, 12)
-ChangePinName(J2, 11, 11)
-ChangePinName(J2, 10, 10)
-ChangePinName(J2, 9, 9)
-ChangePinName(J2, 8, 8)
-ChangePinName(J2, 7, 7)
-ChangePinName(J2, 6, 6)
-ChangePinName(J2, 5, 5)
-ChangePinName(J2, 4, 4)
-ChangePinName(J2, 3, 3)
-ChangePinName(J2, 2, 2)
-ChangePinName(J2, 1, 1)
