e7e69c02b35168c71c12897edf3c6265b99f044d
Michael McMaster <michael@codesrc.com>
2015-05-05 20:15:22 +1000

    Add missing SWV trace functions.

--- lib/SCSI2SD/software/SCSI2SD/src/trace.h
+++ lib/SCSI2SD/software/SCSI2SD/src/trace.h
@@ -1,4 +1,4 @@
-//	Copyright (C) 2015 Michael McMaster <michael@codesrc.com>
+//	Copyright (C) 2015 James Laird-Wah <james@laird-wah.net>
 //
 //	This file is part of SCSI2SD.
 //
@@ -14,11 +14,63 @@
 //
 //	You should have received a copy of the GNU General Public License
 //	along with SCSI2SD.  If not, see <http://www.gnu.org/licenses/>.
-#ifndef TRACE_H
-#define TRACE_H
 
-#define traceInit()
-#define trace(x)
-#define traceIrq(x)
+// Trace event IDs to be output. 1 and 9 are generated as headers on ports 0
+// and 1 respectively, and should not be used.
+enum trace_event {
+	trace_begin = 0,
 
+	// function entries - SCSI
+	trace_scsiRxCompleteISR = 0x50,
+	trace_scsiTxCompleteISR,
+	trace_scsiResetISR,
+	trace_doRxSingleDMA,
+	trace_doTxSingleDMA,
+	trace_scsiPhyReset,
+
+	// spin loops - SCSI
+	trace_spinTxComplete = 0x20,
+	trace_spinReadDMAPoll,
+	trace_spinWriteDMAPoll,
+	trace_spinPhyTxFifo,
+	trace_spinPhyRxFifo,
+	trace_spinDMAReset,
+
+	// SD
+	trace_spinSpiByte = 0x30,
+	trace_spinSDRxFIFO,
+	trace_spinSDBusy,
+	trace_spinSDDMA,
+	trace_spinSDCompleteWrite,
+	trace_spinSDCompleteRead,
+
+	// completion
+	trace_sdSpiByte = 0x40,
+};
+
+void traceInit(void);
+
+#ifdef TRACE
+// normally the code spins waiting for the trace FIFO to be ready for each event
+// if you are debugging a timing-sensitive problem, define TRACE_IMPATIENT and
+// expect some dropped packets
+#ifdef TRACE_IMPATIENT
+    #define wait_fifo(port)  ;
+#else
+    #define wait_fifo(port)  while (!(ITM->PORT[port].u32));
+#endif
+
+	#include <core_cm3_psoc5.h>
+	static inline void trace(enum trace_event ch) {
+		wait_fifo(0);
+		ITM->PORT[0].u8 = ch;
+	}
+	// use a different stimulus port for ISRs to avoid a race
+	static inline void traceIrq(enum trace_event ch) {
+		wait_fifo(1);
+		ITM->PORT[1].u8 = ch;
+	}
+#else
+	#define trace(ev)
+	#define traceIrq(ev)
 #endif
