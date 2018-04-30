12051d0b2de95c54643abfaea4afadb300223016
Michael McMaster <michael@codesrc.com>
2018-04-30 21:54:02 +1000

    Fix bug in scsi self-test function

--- lib/SCSI2SD/src/firmware/config.c
+++ lib/SCSI2SD/src/firmware/config.c
@@ -38,7 +38,7 @@
 
 #include <string.h>
 
-static const uint16_t FIRMWARE_VERSION = 0x0613;
+static const uint16_t FIRMWARE_VERSION = 0x0614;
 
 // 1 flash row
 static const uint8_t DEFAULT_CONFIG[128] =
