3c62f0dc7a7579e73bfc035f62561f60a1afec93
Michael McMaster <michael@codesrc.com>
2019-05-29 22:10:55 +1000

    Prepare for release

--- lib/SCSI2SD/src/firmware/config.c
+++ lib/SCSI2SD/src/firmware/config.c
@@ -37,7 +37,7 @@
 
 #include <string.h>
 
-static const uint16_t FIRMWARE_VERSION = 0x0624;
+static const uint16_t FIRMWARE_VERSION = 0x0625;
 
 // 1 flash row
 static const uint8_t DEFAULT_CONFIG[128] =
