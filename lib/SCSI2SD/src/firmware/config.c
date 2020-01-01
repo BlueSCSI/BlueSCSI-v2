7c7d4d8da3940bd9ac0dfada6611b3e7d57618e0
Michael McMaster <michael@codesrc.com>
2020-01-01 18:59:42 +1000

    64K Data Corruption fix

--- lib/SCSI2SD/src/firmware/config.c
+++ lib/SCSI2SD/src/firmware/config.c
@@ -37,7 +37,7 @@
 
 #include <string.h>
 
-static const uint16_t FIRMWARE_VERSION = 0x062A;
+static const uint16_t FIRMWARE_VERSION = 0x062E;
 
 // Optional static config
 extern uint8_t* __fixed_config;
