7dad748a6791c2b4ee0f7a1f510fabcc346ff829
Michael McMaster <michael@codesrc.com>
2021-08-10 19:45:44 +1000

    Update for release

--- lib/SCSI2SD/src/firmware/config.c
+++ lib/SCSI2SD/src/firmware/config.c
@@ -36,7 +36,7 @@
 
 #include <string.h>
 
-static const uint16_t FIRMWARE_VERSION = 0x064B;
+static const uint16_t FIRMWARE_VERSION = 0x064C;
 
 // Optional static config
 extern uint8_t* __fixed_config;
