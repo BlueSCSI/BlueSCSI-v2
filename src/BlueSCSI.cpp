b0799bc327da962b2a344364009e7cf4f7d33f08
Eric Helgeson <erichelgeson@gmail.com>
2022-05-21 13:21:21 -0500

    Remove tabs

--- src/BlueSCSI.cpp
+++ src/BlueSCSI.cpp
@@ -226,9 +226,9 @@ SdFs SD;
 // HDD image
 typedef struct hddimg_struct
 {
-	FsFile      m_file;                 // File object
-	uint64_t    m_fileSize;             // File size
-	size_t      m_blocksize;            // SCSI BLOCK size
+  FsFile      m_file;                 // File object
+  uint64_t    m_fileSize;             // File size
+  size_t      m_blocksize;            // SCSI BLOCK size
 }HDDIMG;
 HDDIMG  img[NUM_SCSIID][NUM_SCSILUN]; // Maximum number
 
