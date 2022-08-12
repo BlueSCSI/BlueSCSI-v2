918ff1ac959f909eb6c1045d538ae7c7e0e24d66
J. Morio Sakaguchi <morio.earth@gmail.com>
2022-08-12 16:45:54 -0700

    Bump version number for 1.0.5-release
    Cherry picked dev commits
    https://github.com/rabbitholecomputing/ZuluSCSI-firmware/commit/a0898e045912a1501eb895ea4df79ce57eff952e
    https://github.com/rabbitholecomputing/ZuluSCSI-firmware/commit/9ea81a326f93285d6627b093ce28feee77749e47

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -4,7 +4,7 @@
 #pragma once
 
 // Use variables for version number
-#define FW_VER_NUM      "1.0.4"
+#define FW_VER_NUM      "1.0.5"
 #define FW_VER_SUFFIX   "release"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
