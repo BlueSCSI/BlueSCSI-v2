85b28d6cc233db98822559673bc7da46fb416fbc
Alex Perez <aperez@rabbithole.ws>
2022-09-15 11:26:36 -0700

    rc2-->rc3

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -5,7 +5,7 @@
 
 // Use variables for version number
 #define FW_VER_NUM      "1.0.8"
-#define FW_VER_SUFFIX   "rc2"
+#define FW_VER_SUFFIX   "rc3"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
 // Configuration and log file paths
