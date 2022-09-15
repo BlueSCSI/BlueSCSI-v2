064ccdf196437f61332d546dbca3ed268f40e842
Alex Perez <aperez@rabbithole.ws>
2022-09-15 15:54:42 -0700

    Prepare for 1.0.8 release

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -5,7 +5,7 @@
 
 // Use variables for version number
 #define FW_VER_NUM      "1.0.8"
-#define FW_VER_SUFFIX   "rc3"
+#define FW_VER_SUFFIX   "release"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
 // Configuration and log file paths
