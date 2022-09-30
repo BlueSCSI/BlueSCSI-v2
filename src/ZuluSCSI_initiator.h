322870b420b16bd42331ee13067d9e33250b7676
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2022-09-30 23:09:39 +0300

    Initiator mode: handle UNIT ATTENTION state

--- src/ZuluSCSI_initiator.h
+++ src/ZuluSCSI_initiator.h
@@ -25,6 +25,12 @@ bool scsiRequestSense(int target_id, uint8_t *sense_key);
 // Execute UNIT START STOP command to load/unload media
 bool scsiStartStopUnit(int target_id, bool start);
 
+// Execute INQUIRY command
+bool scsiInquiry(int target_id, uint8_t inquiry_data[36]);
+
+// Execute TEST UNIT READY command and handle unit attention state
+bool scsiTestUnitReady(int target_id);
+
 // Read a block of data from SCSI device and write to file on SD card
 class FsFile;
 bool scsiInitiatorReadDataToFile(int target_id, uint32_t start_sector, uint32_t sectorcount, uint32_t sectorsize,
