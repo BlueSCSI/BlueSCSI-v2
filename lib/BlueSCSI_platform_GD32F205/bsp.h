1757b6c3f3eb2260981862ff7fa72fb7aa4b7165
Eric Helgeson <erichelgeson@gmail.com>
2022-11-04 12:38:38 -0500

    Remove GD32

--- lib/BlueSCSI_platform_GD32F205/bsp.h
+++
@@ -1,5 +0,0 @@
-// Dummy file for SCSI2SD.
-
-#pragma once
-
-#define S2S_DMA_ALIGN
--- lib/BlueSCSI_platform_GD32F205/scsi_accel_asm.h
+++
@@ -1,8 +0,0 @@
-// SCSI subroutines using hand-optimized assembler
-
-#pragma once
-
-#include <stdint.h>
-
-void scsi_accel_asm_send(const uint32_t *buf, uint32_t num_words, volatile int *resetFlag);
-void scsi_accel_asm_recv(uint32_t *buf, uint32_t num_words, volatile int *resetFlag);
