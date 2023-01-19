1ee257abf307dd36a50180f656aaf26c0ae8ef36
Eric Helgeson <erichelgeson@gmail.com>
2023-01-19 08:21:04 -0600

    We find more than just hard disk drive images

--- src/BlueSCSI.cpp
+++ src/BlueSCSI.cpp
@@ -193,7 +193,7 @@ bool findHDDImages()
   int dirindex = 0;
 
   log(" ");
-  log("=== Finding HDD images in ", imgdir, " ===");
+  log("=== Finding images in ", imgdir, " ===");
 
   SdFile root;
   root.open(imgdir);
