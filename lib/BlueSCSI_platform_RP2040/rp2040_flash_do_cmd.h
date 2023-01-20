d268c7f960759c523ff5ec6430f49158e7c9eb4c
Eric Helgeson <erichelgeson@gmail.com>
2023-01-20 11:13:25 -0600

    Per the GPL's FAQ: https://www.gnu.org/licenses/gpl-faq.en.html#NoticeInSourceFile
    I have added the appropreate GPL header to each file. If there was an
    existing copyright line it was kept.
    
    Since there are so many contributors through these projects I've elected
    to put them all in a CONTRIBUTORS.txt file. This file will be updated as
    new contributors add to this project.
    
    There is no intent to remove any copyright notices. Care was taken
    to ensure each file in this repository is correctly licensed. If any
    mistakes were made in this or future commits I will address them as soon
    as possible. Any changes or clarifications by upstream projects will be
    incorperated.
    
    Files autogenerated by tools or derived from the Raspery Pi foundation
    were left as is. It is unclear how the licensing should work in
    these cases.

--- lib/BlueSCSI_platform_RP2040/rp2040_flash_do_cmd.h
+++ lib/BlueSCSI_platform_RP2040/rp2040_flash_do_cmd.h
@@ -1,3 +1,21 @@
+/*
+BlueSCSI
+Copyright (c) 2022-2023 the BlueSCSI contributors (CONTRIBUTORS.txt)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // As of 2022-11-16, the raspberrypi platformio package ships with old version
 // of pico-sdk. This version lacks the flash_do_cmd() function in flash.h header.
 // This file backports the function from new versions.
