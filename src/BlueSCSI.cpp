56e55d73610bdf01270f9c53ec9a6b037b833466
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2023-02-09 12:40:30 +0200

    Cache zuluscsi.ini file contents to speed up booting.
    This cuts about 1 second from boot time by only reading the ini
    once on boot and after SD card hotplug.

--- src/BlueSCSI.cpp
+++ src/BlueSCSI.cpp
@@ -47,6 +47,7 @@
 
 #include <SdFat.h>
 #include <minIni.h>
+#include <minIni_cache.h>
 #include <string.h>
 #include <strings.h>
 #include <ctype.h>
@@ -491,9 +492,14 @@ void readSCSIDeviceConfig()
 
 static bool mountSDCard()
 {
+  invalidate_ini_cache();
+
   // Check for the common case, FAT filesystem as first partition
   if (SD.begin(SD_CONFIG))
+  {
+    reload_ini_cache(CONFIGFILE);
     return true;
+  }
 
   // Do we have any kind of card?
   if (!SD.card() || SD.sdErrorCode() != 0)
