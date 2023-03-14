4aca7785b9b0cb51d84ea6a69c2dba702f6036a9
Alex Perez <aperez@rabbithole.ws>
2023-03-14 11:17:58 -0700

    Iterate vernum post-release

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -6,8 +6,8 @@
 #include <ZuluSCSI_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2023.03.14"
-#define FW_VER_SUFFIX   "release"
+#define FW_VER_NUM      "2023.03.15"
+#define FW_VER_SUFFIX   "devel"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
 // Configuration and log file paths
