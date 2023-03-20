6d99dfee974dbc5698e590c68485d579edf0d432
Morio <morio.earth@gmail.com>
2023-03-20 12:17:55 -0700

    Added copyrights to ZuluSCSI code

--- src/ZuluSCSI_disk.cpp
+++ src/ZuluSCSI_disk.cpp
@@ -1,10 +1,30 @@
+/** 
+ * SCSI2SD V6 - Copyright (C) 2013 Michael McMaster <michael@codesrc.com>
+ * Copyright (C) 2014 Doug Brown <doug@downtowndougbrown.com
+ * ZuluSCSI™ - Copyright (c) 2022 Rabbit Hole Computing™
+ *  
+ * This file is licensed under the GPL version 3 or any later version. 
+ * It is derived from disk.c in SCSI2SD V6
+ * 
+ * https://www.gnu.org/licenses/gpl-3.0.html
+ * ----
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**/
+
+
 // This file implements the main SCSI disk emulation and data streaming.
 // It is derived from disk.c in SCSI2SD V6.
-//
-//    Licensed under GPL v3.
-//    Copyright (C) 2013 Michael McMaster <michael@codesrc.com>
-//    Copyright (C) 2014 Doug Brown <doug@downtowndougbrown.com>
-//    Copyright (C) 2022 Rabbit Hole Computing
 
 #include "ZuluSCSI_disk.h"
 #include "ZuluSCSI_log.h"
--- utils/run_gdb_v1_1.sh
+++ utils/run_gdb_v1_1.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# ZuluSCSI™ - Copyright (c) 2022 Rabbit Hole Computing™
+#
+# ZuluSCSI™ file is licensed under the GPL version 3 or any later version. 
+#
+# https://www.gnu.org/licenses/gpl-3.0.html
+# ----
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version. 
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details. 
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 # This script runs GDB with openocd and stlink to
 # allow debugging and seeing the SWO log output in realtime.
 
