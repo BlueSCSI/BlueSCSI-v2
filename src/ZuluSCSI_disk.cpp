d511f110427386573462a059b25e4c54c37f3803
Alex Perez <aperez@rabbithole.ws>
2023-03-24 13:03:03 -0700

    Revert "Merge pull request #159 from ZuluSCSI/boot_delay_improvement"
    This reverts commit 26d1f7552579224e04011b283667298d1abf703d, reversing
    changes made to 1d0a7fb41808441655deb601cb19af6b4148f33c.

--- src/ZuluSCSI_disk.cpp
+++ src/ZuluSCSI_disk.cpp
@@ -100,7 +100,6 @@ bool scsiIsReadFinished(const uint8_t *data)
 #endif
 
 // SD card sector size is always 512 bytes
-extern SdFs SD;
 #define SD_SECTOR_SIZE 512
 
 /************************************************/
