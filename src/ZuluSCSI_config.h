7e2db393a40a007b27e2843e14c69c53d5f9db69
Alex Perez <aperez@rabbithole.ws>
2023-03-30 09:48:53 -0700

    Prepare RP2040-specific bugfix release

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -27,8 +27,8 @@
 #include <ZuluSCSI_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "23.03.29"
-#define FW_VER_SUFFIX   "devel"
+#define FW_VER_NUM      "23.03.30"
+#define FW_VER_SUFFIX   "release"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
 // Configuration and log file paths
