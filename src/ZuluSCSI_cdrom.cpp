BlueSCSI/BlueSCSI-v2

commit f964710c6885a58e5f9f411dc1f36c160386d51d

Intercept READ(6) to allow CHECK CONDITION when blocksize != 2048

src/ZuluSCSI_cdrom.cpp
@@ -1708,6 +1708,17 @@ extern "C" int scsiCDRomCommand()
 
         doReadSubchannel(time, subq, parameter, track_number, allocationLength);
     }
+    else if (command == 0x08)
+    {
+        // READ(6) for CDs (may need sector translation for cue file handling)
+        uint32_t lba =
+            (((uint32_t) scsiDev.cdb[1] & 0x1F) << 16) +
+            (((uint32_t) scsiDev.cdb[2]) << 8) +
+            scsiDev.cdb[3];
+        uint32_t blocks = scsiDev.cdb[4];
+
+        doReadCD(lba, blocks, 0, 0x10, 0, true);
+    }
     else if (command == 0x28)
     {
         // READ(10) for CDs (may need sector translation for cue file handling)

commit a9e447b25cce194b4c7c332abfdbe5e8f9112045

Fix regression in CD-ROM READ(6) command (#231)
Block count of 0 means 256 blocks in SCSI READ(6) command.
This bug was introduced in commit f964710 and is present
in versions 2023-05-24 and 2023-05-25.

src/ZuluSCSI_cdrom.cpp
@@ -1741,6 +1741,7 @@ extern "C" int scsiCDRomCommand()
             (((uint32_t) scsiDev.cdb[2]) << 8) +
             scsiDev.cdb[3];
         uint32_t blocks = scsiDev.cdb[4];
+        if (blocks == 0) blocks = 256;
 
         doReadCD(lba, blocks, 0, 0x10, 0, true);
     }