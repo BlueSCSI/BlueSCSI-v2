c3b877ce708e226527c81198904fa8e2daf55d6c
Morio <morio.earth@gmail.com>
2023-08-21 14:34:20 -0700

    SCSI interface working
    Getting normal transfer rates for an RP2040 on the SCSI interface.
    Added a flag to disable the SWO pin (DISABLE_SWO). Not sure if the
    define should be in the platformio.ini file or in the
    ZuluSCSI_platform_RP2040 library.

--- lib/ZuluSCSI_platform_RP2040/scsi_accel_Pico.pio.h
+++ lib/ZuluSCSI_platform_RP2040/scsi_accel_Pico.pio.h
@@ -50,8 +50,8 @@ static const uint16_t scsi_accel_async_write_program_instructions[] = {
     0x90e0, //  0: pull   ifempty block   side 1     
     0x7009, //  1: out    pins, 9         side 1     
     0x7577, //  2: out    null, 23        side 1 [5] 
-    0x308a, //  3: wait   1 gpio, 10      side 1     
-    0x200a, //  4: wait   0 gpio, 10      side 0     
+    0x309a, //  3: wait   1 gpio, 26      side 1     
+    0x201a, //  4: wait   0 gpio, 26      side 0     
             //     .wrap
 };
 
@@ -80,9 +80,9 @@ static inline pio_sm_config scsi_accel_async_write_program_get_default_config(ui
 static const uint16_t scsi_accel_read_program_instructions[] = {
             //     .wrap_target
     0x90a0, //  0: pull   block           side 1     
-    0x308a, //  1: wait   1 gpio, 10      side 1     
+    0x309a, //  1: wait   1 gpio, 26      side 1     
     0x4061, //  2: in     null, 1         side 0     
-    0x200a, //  3: wait   0 gpio, 10      side 0     
+    0x201a, //  3: wait   0 gpio, 26      side 0     
     0x5009, //  4: in     pins, 9         side 1     
     0x5056, //  5: in     y, 22           side 1     
             //     .wrap
@@ -142,8 +142,8 @@ static inline pio_sm_config scsi_sync_write_program_get_default_config(uint offs
 
 static const uint16_t scsi_sync_write_pacer_program_instructions[] = {
             //     .wrap_target
-    0x208a, //  0: wait   1 gpio, 10                 
-    0x200a, //  1: wait   0 gpio, 10                 
+    0x209a, //  0: wait   1 gpio, 26                 
+    0x201a, //  1: wait   0 gpio, 26                 
     0x6061, //  2: out    null, 1                    
             //     .wrap
 };
