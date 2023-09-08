f9e089ac443f5a3b22753749514d87078e38fc86
Alex Perez <aperez@rabbithole.ws>
2023-09-07 18:23:27 -0700

    Update ZuluSCSI_config.h

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -28,7 +28,7 @@
 
 // Use variables for version number
 #define FW_VER_NUM      "23.08.08"
-#define FW_VER_SUFFIX   "devel"
+#define FW_VER_SUFFIX   "pico"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
 // Configuration and log file paths
