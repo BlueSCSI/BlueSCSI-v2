ea7d62c660b427b61151faebc6785a2a36eed152
Alex Perez <aperez@rabbithole.ws>
2023-09-21 10:27:35 -0700

    Update ZuluSCSI_config.h

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -27,7 +27,7 @@
 #include <ZuluSCSI_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "23.09.20"
+#define FW_VER_NUM      "23.09.21"
 #define FW_VER_SUFFIX   "devel"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
