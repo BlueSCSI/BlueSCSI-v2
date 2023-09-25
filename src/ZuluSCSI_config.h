573dc60ca91c54363a0bdad20ba37e3029807c6e
Morio <morio.earth@gmail.com>
2023-09-25 12:17:59 -0700

    Output log over serial USB
    Added logging over serial USB to the DaynaPORT target.
    Removed the DISABLE_USB macros.

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -28,8 +28,8 @@
 #include <ZuluSCSI_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "23.09.21"
-#define FW_VER_SUFFIX   "devel"
+#define FW_VER_NUM      "23.09.25"
+#define FW_VER_SUFFIX   "wifi-dev"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
 // Configuration and log file paths
