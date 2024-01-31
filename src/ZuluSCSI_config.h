813c65c48aa320350f8f7d5f9c400d2a827162c3
Alex Perez <aperez@rabbithole.ws>
2024-01-31 15:55:42 -0800

    Iterate version string

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -28,7 +28,7 @@
 #include <ZuluSCSI_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "24.01.24"
+#define FW_VER_NUM      "24.01.31"
 #define FW_VER_SUFFIX   "dev"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
