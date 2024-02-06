4a48beb9715f2feb379d3871dafe8440019e473e
Alex Perez <aperez@rabbithole.ws>
2024-02-05 16:19:41 -0800

    Prepare

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -29,7 +29,7 @@
 
 // Use variables for version number
 #define FW_VER_NUM      "24.02.05"
-#define FW_VER_SUFFIX   "dev"
+#define FW_VER_SUFFIX   "release"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
 // Configuration and log file paths
