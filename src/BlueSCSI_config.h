d63dd819b4169371ac5f854cd13d8e6a3e68d62a
Eric Helgeson <erichelgeson@gmail.com>
2024-02-22 16:18:08 -0600

    dev

--- src/BlueSCSI_config.h
+++ src/BlueSCSI_config.h
@@ -6,8 +6,8 @@
 #include <BlueSCSI_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2024.02.22"
-#define FW_VER_SUFFIX   "release"
+#define FW_VER_NUM      "2024.02.23"
+#define FW_VER_SUFFIX   "dev"
 #define BLUESCSI_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
 // Configuration and log file paths
