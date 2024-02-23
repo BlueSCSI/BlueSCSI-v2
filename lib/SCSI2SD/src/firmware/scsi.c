0857bc6b68f535a8a3c465edbfdaf629e631a711
Eric Helgeson <erichelgeson@gmail.com>
2024-02-23 16:45:17 -0600

    scsi: inquiry should respond to invalid LUNs and set peripheral qualifier and peripheral device-type fields to 0x7F. Fixes #127

--- lib/SCSI2SD/src/firmware/scsi.c
+++ lib/SCSI2SD/src/firmware/scsi.c
@@ -413,14 +413,7 @@ static void process_Command()
 	}
 	else if (command == 0x12)
 	{
-		if(scsiDev.lun)
-		{
-			scsiDev.target->sense.code = ILLEGAL_REQUEST;
-			scsiDev.target->sense.asc = LOGICAL_UNIT_NOT_SUPPORTED;
-			enter_Status(CHECK_CONDITION);
-		} else {
-			s2s_scsiInquiry();
-		}
+		s2s_scsiInquiry();
 	}
 	else if (command == 0x03)
 	{
