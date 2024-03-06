7b5350bbdc0e26984112f73abf72a4c8fa04f797
Morio <morio.earth@gmail.com>
2024-03-05 16:52:42 -0800

    Update copyright headers

--- lib/SCSI2SD/src/firmware/scsi.c
+++ lib/SCSI2SD/src/firmware/scsi.c
@@ -1,6 +1,7 @@
 //	Copyright (C) 2014 Michael McMaster <michael@codesrc.com>
 //	Copyright (c) 2023 joshua stein <jcs@jcs.org>
 //	Copyright (c) 2023 Andrea Ottaviani <andrea.ottaviani.69@gmail.com>
+//	Copyright (C) 2024 Rabbit Hole Computing LLC
 //
 //	This file is part of SCSI2SD.
 //
