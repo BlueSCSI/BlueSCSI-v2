1dacc86135a1f450294344d0969fbcb1d6efcb4b
Eric Helgeson <erichelgeson@gmail.com>
2024-09-23 19:24:04 -0500

    scsi: allow for switching of any removable media

--- src/BlueSCSI_disk.h
+++ src/BlueSCSI_disk.h
@@ -2,6 +2,7 @@
  * SCSI2SD V6 - Copyright (C) 2013 Michael McMaster <michael@codesrc.com>
  * Copyright (C) 2014 Doug Brown <doug@downtowndougbrown.com
  * ZuluSCSI™ - Copyright (c) 2022 Rabbit Hole Computing™
+ * Copyright (C) 2024 Eric Helgeson <erichelgeson@gmail.com>
  * 
  * It is derived from disk.h in SCSI2SD V6.
  * 
@@ -129,7 +130,7 @@ bool scsiDiskCheckAnyImagesConfigured();
 // images. As a side effect this advances image tracking to the next image.
 // Returns the length of the new image filename, or 0 if the target is not
 // configured for multiple images.
-int scsiDiskGetNextImageName(image_config_t &img, char *buf, size_t buflen);
+int scsiDiskGetNextImageName(image_config_t &img, char *buf, size_t buf_len);
 
 // Get pointer to extended image configuration based on target idx
 image_config_t &scsiDiskGetImageConfig(int target_idx);
@@ -146,3 +147,9 @@ bool scsiDiskCheckAnyNetworkDevicesConfigured();
 
 // Switch to next Drive image if multiple have been configured
 bool switchNextImage(image_config_t &img, const char* next_filename = nullptr);
+
+// For removable, non-CD based images
+void removableInsert(image_config_t &img);
+
+// For removable, non-CD based images
+void removableEject(image_config_t &img);
