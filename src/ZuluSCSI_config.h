54a0bdec9ac191fd29e5fd15286a3db7c703718a
Alex Perez <aperez@rabbithole.ws>
2024-09-27 17:24:57 -0700

    Update ZuluSCSI_config.h

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -28,8 +28,8 @@
 #include <ZuluSCSI_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "24.08.22"
-#define FW_VER_SUFFIX   "release"
+#define FW_VER_NUM      "24.09.27"
+#define FW_VER_SUFFIX   "pico2-alpha"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 #define INQUIRY_NAME  PLATFORM_NAME " v" ZULU_FW_VERSION
 #define TOOLBOX_API 0
