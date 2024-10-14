6f4f7921e630c3373263bc33c33f9579ccd417a6
androda <3105206+androda@users.noreply.github.com>
2024-10-14 06:30:24 -0600

    Setting To Ignore Parity For Initiator Mode Data Transfers

--- lib/BlueSCSI_platform_RP2040/scsi_accel_host.h
+++ lib/BlueSCSI_platform_RP2040/scsi_accel_host.h
@@ -8,4 +8,4 @@ void scsi_accel_host_init();
 
 // Read data from SCSI bus.
 // Number of bytes to read must be divisible by two.
-uint32_t scsi_accel_host_read(uint8_t *buf, uint32_t count, int *parityError, volatile int *resetFlag);
+uint32_t scsi_accel_host_read(uint8_t *buf, uint32_t count, int *parityError, uint32_t *parityResult, volatile int *resetFlag);
