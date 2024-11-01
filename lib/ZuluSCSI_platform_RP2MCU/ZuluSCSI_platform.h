bd746d3946eec4414d3671f256ab2c9cead4e4e5
J. Morio Sakaguchi <morio.earth@gmail.com>
2024-11-01 13:27:30 -0700

    Add ability to reset via terminal for RP2 boards
    Pressing `m` or `r` then `y` will reboot the board. If `m` is pressed
    the board will reboot into mass storage.
    
    This will be useful to users playing with their timings or modifying
    their `zuluscsi.ini` settings.
    
    For users modifying their timings, they will need to reboot into mass
    storage, edit their `zulu_timings.ini` file, then do an 'r' reboot to
    reclock their board with their new settings.
    
    Most zuluscsi.ini file settings get reloaded after ejecting the
    mass storage device.

--- lib/ZuluSCSI_platform_RP2MCU/ZuluSCSI_platform.h
+++ lib/ZuluSCSI_platform_RP2MCU/ZuluSCSI_platform.h
@@ -1,20 +1,20 @@
-/** 
+/**
  * ZuluSCSI™ - Copyright (c) 2022 Rabbit Hole Computing™
- * 
- * ZuluSCSI™ firmware is licensed under the GPL version 3 or any later version. 
- * 
+ *
+ * ZuluSCSI™ firmware is licensed under the GPL version 3 or any later version.
+ *
  * https://www.gnu.org/licenses/gpl-3.0.html
  * ----
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version. 
- * 
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details. 
- * 
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 **/
@@ -115,6 +115,9 @@ uint32_t platform_sys_clock_in_hz();
 
 // Attempt to reclock the MCU
 zuluscsi_reclock_status_t platform_reclock(uint32_t clk_in_khz);
+
+// Returns true if reboot was for mass storage
+bool platform_rebooted_into_mass_storage();
 
 // Set callback that will be called during data transfer to/from SD card.
 // This can be used to implement simultaneous transfer to SCSI bus.
