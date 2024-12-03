8133713e4d77e287ad99484746bc7e840863c3bb
Tony Kuker <akuker@gmail.com>
2024-12-02 20:51:56 -0600

    add more cli commands and cleanup

---
+++ src_bridge/src/freertos_cli_commands.h
@@ -0,0 +1,12 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * The function that registers the commands that are defined within this file.
+ */
+void vRegisterSampleCLICommands( void );
+
+#ifdef __cplusplus
+}
+#endif
