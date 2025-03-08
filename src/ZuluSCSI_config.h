e63e5d9de85a35ee735a787c5d6f3318986bf4f4
Morio <morio.earth@gmail.com>
2025-03-07 17:31:22 -0800

    Bump firmware version

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -27,8 +27,8 @@
 #include <ZuluSCSI_platform_config.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "25.03.04"
-#define FW_VER_SUFFIX   "beta1"
+#define FW_VER_NUM      "25.03.08"
+#define FW_VER_SUFFIX   "rc1"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
 #define STRINGFY(STR) #STR
