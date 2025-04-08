6c7ad88b1533887b5289c21f6ff120a6ad055c97
Alex Perez <aperez@rabbithole.ws>
2025-04-08 08:10:59 -0700

    Back to dev

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -27,8 +27,8 @@
 #include <ZuluSCSI_platform_config.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "25.04.01"
-#define FW_VER_SUFFIX   "release"
+#define FW_VER_NUM      "25.04.08"
+#define FW_VER_SUFFIX   "dev"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
 #define STRINGFY(STR) #STR
