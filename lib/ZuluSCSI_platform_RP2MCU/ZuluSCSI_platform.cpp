BlueSCSI/BlueSCSI-v2

commit 3b0bb3eb8e1302eb041bfad552a200ad85d5e76d

RP2MCU: Check USB serial connection only once per second
This avoids putting the USB overhead into every loop through
platform_poll() if USB is not connected.

When USB is connected, it still gets served every loop.

lib/ZuluSCSI_platform_RP2MCU/ZuluSCSI_platform.cpp
@@ -645,6 +645,28 @@ void isr_hardfault(void)
 /* Debug logging and watchdog            */
 /*****************************************/
 
+static bool usb_serial_connected()
+{
+#ifdef PIO_FRAMEWORK_ARDUINO_NO_USB
+    return false;
+#endif
+
+    static bool connected;
+    static uint32_t last_check_time;
+
+#ifdef PLATFORM_MASS_STORAGE
+    if (platform_msc_lock_get()) return connected; // Avoid re-entrant USB events
+#endif
+
+    if (last_check_time == 0 || (uint32_t)(millis() - last_check_time) > 1000)
+    {
+        connected = bool(Serial);
+        last_check_time = millis();
+    }
+
+    return connected;
+}
+
 // Send log data to USB UART if USB is connected.
 // Data is retrieved from the shared log ring buffer and
 // this function sends as much as fits in USB CDC buffer.
@@ -658,6 +680,8 @@ static void usb_log_poll()
 {
 #ifndef PIO_FRAMEWORK_ARDUINO_NO_USB
     static uint32_t logpos = 0;
+
+    if (!usb_serial_connected()) return;
 
 #ifdef PLATFORM_MASS_STORAGE
     if (platform_msc_lock_get()) return; // Avoid re-entrant USB events
@@ -686,7 +710,9 @@ static void usb_log_poll()
 // Grab input from USB Serial terminal
 static void usb_input_poll()
 {
-    #ifndef PIO_FRAMEWORK_ARDUINO_NO_USB
+#ifndef PIO_FRAMEWORK_ARDUINO_NO_USB
+
+    if (!usb_serial_connected()) return;
 
 #ifdef PLATFORM_MASS_STORAGE
     if (platform_msc_lock_get()) return; // Avoid re-entrant USB events

commit 8032db153071fc53be38e94407118935134bf70c

RP2MCU: Fix USB console not connecting
There was a regression caused by commit 3b0bb3eb8e1302e which
caused the USB console not to connect after the first SCSI request
had been processed.

This seems to be related to the USB request timeout of 500 ms, where
the polling interval was previously 1000 ms.

I didn't research deep enough to find why this works before the first
SCSI request, nor why it doesn't work afterwards. The USB IRQ still
seems to be called so it has something to do with the userspace
processing of requests. Possibly the USB MSC support also affects
it.

In any case, lowering the polling interval to 50 ms fixed the issue
and gives just as good performance.

lib/ZuluSCSI_platform_RP2MCU/ZuluSCSI_platform.cpp
@@ -665,7 +665,7 @@ static bool usb_serial_connected()
     if (platform_msc_lock_get()) return connected; // Avoid re-entrant USB events
 #endif
 
-    if (last_check_time == 0 || (uint32_t)(millis() - last_check_time) > 1000)
+    if (last_check_time == 0 || (uint32_t)(millis() - last_check_time) > 50)
     {
         connected = bool(Serial);
         last_check_time = millis();