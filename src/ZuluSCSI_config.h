b14701a2eb44700a9e73fa8dd315afe07929012f
Alex Perez <aperez@rabbithole.ws>
2025-05-12 14:39:56 -0700

    Prepare for v2025.05.12 bugfix release

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -27,7 +27,7 @@
 #include <ZuluSCSI_platform_config.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "25.05.01"
+#define FW_VER_NUM      "25.05.12"
 #define FW_VER_SUFFIX   "release"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
