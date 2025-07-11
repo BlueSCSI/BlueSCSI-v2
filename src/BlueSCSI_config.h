c24be9e9c19a01db03fb8837443554c1034a0fe5
Eric Helgeson <erichelgeson@gmail.com>
2025-07-11 10:45:39 -0500

    v2025.07.11

--- src/BlueSCSI_config.h
+++ src/BlueSCSI_config.h
@@ -29,9 +29,9 @@
 #include <BlueSCSI_platform_config.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2025.07.09"
+#define FW_VER_NUM      "2025.07.11"
 // rel or dev only.
-#define FW_VER_SUFFIX   "dev"
+#define FW_VER_SUFFIX   "rel"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
 #define STRINGFY(STR) #STR
