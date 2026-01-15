82fa20737183448ddfa48c03bb20b3d5f27292ac
Eric Helgeson <erichelgeson@gmail.com>
2026-01-15 10:48:37 -0600

    left over from when they ported BlueSCSI-v1 into this codebase, never used. #lessdefines

--- src/BlueSCSI_config.h
+++ src/BlueSCSI_config.h
@@ -77,7 +77,6 @@
 // SCSI config
 #define NUM_SCSIID  8          // Maximum number of supported SCSI-IDs (The minimum is 0)
 #define NUM_SCSILUN 1          // Maximum number of LUNs supported     (Currently has to be 1)
-#define READ_PARITY_CHECK 0    // Perform read parity check (unverified)
 
 // SCSI raw fallback configuration when no image files are detected
 // Presents the whole SD card as an SCSI drive
