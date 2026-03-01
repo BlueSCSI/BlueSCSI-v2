d4a389553802483866f62e34f95779fdb08ec65a
Eric Helgeson <erichelgeson@gmail.com>
2026-03-01 09:00:09 -0600

    v2026.03.01 #MARCHintosh 2026

--- src/BlueSCSI_config.h
+++ src/BlueSCSI_config.h
@@ -30,9 +30,9 @@
 #include <BlueSCSI_platform_config.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2026.02.09"
+#define FW_VER_NUM      "2026.03.01"
 // rel or dev only.
-#define FW_VER_SUFFIX   "dev"
+#define FW_VER_SUFFIX   "rel"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
 #define STRINGFY(STR) #STR
